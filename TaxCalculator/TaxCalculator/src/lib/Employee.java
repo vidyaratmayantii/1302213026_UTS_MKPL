6bdf05be122e80e67461eea01610e726a4b51abe
vidyaratmayantii <126158623+vidyaratmayantii@users.noreply.github.com>
2024-04-20 20:33:48 +0700

    Memecah class employee
    Mmemcah class employe menjadi class employee, salary grade, spouse dan depent

--- TaxCalculator/TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/TaxCalculator/src/lib/Employee.java
@@ -1,105 +1,78 @@
 package lib;
 
 import java.time.LocalDate;
-import java.time.Month;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Employee {
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private String idNumber;
+    private String address;
+    private LocalDate dateJoined;
+    private boolean isForeigner;
+    private Gender gender;
+    private SalaryGrade salaryGrade;
+    private Spouse spouse;
+    private List<Dependent> dependents;
 
-	private String employeeId;
-	private String firstName;
-	private String lastName;
-	private String idNumber;
-	private String address;
-	
-	private int yearJoined;
-	private int monthJoined;
-	private int dayJoined;
-	private int monthWorkingInYear;
-	
-	private boolean isForeigner;
-	private boolean gender; //true = Laki-laki, false = Perempuan
-	
-	private int monthlySalary;
-	private int otherMonthlyIncome;
-	private int annualDeductible;
-	
-	private String spouseName;
-	private String spouseIdNumber;
+    public Employee(String employeeId, String firstName, String lastName, String idNumber, String address,
+                    LocalDate dateJoined, boolean isForeigner, Gender gender, SalaryGrade salaryGrade) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = idNumber;
+        this.address = address;
+        this.dateJoined = dateJoined;
+        this.isForeigner = isForeigner;
+        this.gender = gender;
+        this.salaryGrade = salaryGrade;
+        this.dependents = new ArrayList<>();
+    }
 
-	private List<String> childNames;
-	private List<String> childIdNumbers;
-	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
-		this.employeeId = employeeId;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.idNumber = idNumber;
-		this.address = address;
-		this.yearJoined = yearJoined;
-		this.monthJoined = monthJoined;
-		this.dayJoined = dayJoined;
-		this.isForeigner = isForeigner;
-		this.gender = gender;
-		
-		childNames = new LinkedList<String>();
-		childIdNumbers = new LinkedList<String>();
-	}
-	
-	/**
-	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
-	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
-	 */
-	
-	public void setMonthlySalary(int grade) {	
-		if (grade == 1) {
-			monthlySalary = 3000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 2) {
-			monthlySalary = 5000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 3) {
-			monthlySalary = 7000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}
-	}
-	
-	public void setAnnualDeductible(int deductible) {	
-		this.annualDeductible = deductible;
-	}
-	
-	public void setAdditionalIncome(int income) {	
-		this.otherMonthlyIncome = income;
-	}
-	
-	public void setSpouse(String spouseName, String spouseIdNumber) {
-		this.spouseName = spouseName;
-		this.spouseIdNumber = idNumber;
-	}
-	
-	public void addChild(String childName, String childIdNumber) {
-		childNames.add(childName);
-		childIdNumbers.add(childIdNumber);
-	}
-	
-	public int getAnnualIncomeTax() {
-		
-		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
-		LocalDate date = LocalDate.now();
-		
-		if (date.getYear() == yearJoined) {
-			monthWorkingInYear = date.getMonthValue() - monthJoined;
-		}else {
-			monthWorkingInYear = 12;
-		}
-		
-		return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouseIdNumber.equals(""), childIdNumbers.size());
-	}
+    public void setSpouse(String spouseName, String spouseIdNumber) {
+        this.spouse = new Spouse(spouseName, spouseIdNumber);
+    }
+
+    public void addChild(String childName, String childIdNumber) {
+        this.dependents.add(new Child(childName, childIdNumber));
+    }
+
+    public BigDecimal calculateMonthlySalary() {
+        BigDecimal baseSalary = salaryGrade.getBaseSalary();
+        if (isForeigner) {
+            baseSalary = baseSalary.multiply(BigDecimal.valueOf(1.5));
+        }
+        return baseSalary;
+    }
+
+    public BigDecimal calculateAnnualIncomeTax() {
+        int monthsWorked = calculateMonthsWorkedThisYear();
+        BigDecimal taxableIncome = calculateTaxableIncome(monthsWorked);
+        BigDecimal taxExemption = calculateTaxExemption();
+        BigDecimal tax = taxableIncome.multiply(BigDecimal.valueOf(0.05)).subtract(taxExemption);
+        return tax.max(BigDecimal.ZERO).setScale(0, RoundingMode.HALF_UP);
+    }
+
+    private int calculateMonthsWorkedThisYear() {
+        LocalDate currentDate = LocalDate.now();
+        return (currentDate.getYear() == dateJoined.getYear()) ?
+               currentDate.getMonthValue() - dateJoined.getMonthValue() + 1 : 12;
+    }
+
+    private BigDecimal calculateTaxableIncome(int monthsWorked) {
+        BigDecimal monthlySalary = calculateMonthlySalary();
+        return monthlySalary.multiply(BigDecimal.valueOf(monthsWorked));
+    }
+
+    private BigDecimal calculateTaxExemption() {
+        BigDecimal taxExemption = BigDecimal.valueOf(54000000);
+        if (spouse != null) {
+            taxExemption = taxExemption.add(BigDecimal.valueOf(4500000));
+        }
+        int childCount = dependents.size();
+        taxExemption = taxExemption.add(BigDecimal.valueOf(Math.min(childCount, 3) * 4500000));
+        return taxExemption;
+    }
 }
