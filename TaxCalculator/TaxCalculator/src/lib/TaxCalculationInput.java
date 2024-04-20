eac10f8532d5350fb49ea69a64ac29cf6d011025
vidyaratmayantii <126158623+vidyaratmayantii@users.noreply.github.com>
2024-04-20 21:53:22 +0700

    Mengganti tipe data primitive
    Mengganti tipe data primitive menjadi objek seperti salary

---
+++ TaxCalculator/TaxCalculator/src/lib/TaxCalculationInput.java
@@ -0,0 +1,43 @@
+package lib;
+
+public class TaxCalculationInput {
+    private Salary monthlySalary;
+    private Salary otherMonthlyIncome;
+    private int numberOfMonthsWorked;
+    private Deductible deductible;
+    private MaritalStatus maritalStatus;
+    private NumberOfChildren numberOfChildren;
+
+    public TaxCalculationInput(Salary monthlySalary, Salary otherMonthlyIncome, int numberOfMonthsWorked, Deductible deductible, MaritalStatus maritalStatus, NumberOfChildren numberOfChildren) {
+        this.monthlySalary = monthlySalary;
+        this.otherMonthlyIncome = otherMonthlyIncome;
+        this.numberOfMonthsWorked = numberOfMonthsWorked;
+        this.deductible = deductible;
+        this.maritalStatus = maritalStatus;
+        this.numberOfChildren = numberOfChildren;
+    }
+
+    public Salary getMonthlySalary() {
+        return monthlySalary;
+    }
+
+    public Salary getOtherMonthlyIncome() {
+        return otherMonthlyIncome;
+    }
+
+    public int getNumberOfMonthsWorked() {
+        return numberOfMonthsWorked;
+    }
+
+    public Deductible getDeductible() {
+        return deductible;
+    }
+
+    public MaritalStatus getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    public NumberOfChildren getNumberOfChildren() {
+        return numberOfChildren;
+    }
+}
